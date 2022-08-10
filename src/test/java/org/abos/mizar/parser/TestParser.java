41030d59a9705e60a9fb350499da02653e5ff42f
ayutac <fly.high.android@gmail.com>
2022-08-10 05:59:39 +0200

    added Parser test

--- src/test/java/org/abos/mizar/parser/TestParser.java
+++ src/test/java/org/abos/mizar/parser/TestParser.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 public class TestParser {
 
     @Test
-    public void testEmptyArticle() throws IOException {
+    public void testEmptyArticle() throws IOException, ParseException {
         String emptyArticleStr = Parser.loadFromFile("/fixtures/articles/emptyArticle.miz");
         Article emptyArticle = new Parser().parse("EMPTY", emptyArticleStr);
         Assertions.assertTrue(emptyArticle.getEnviron().getVocabularies().isEmpty());
@@ -23,6 +23,17 @@ public class TestParser {
         Assertions.assertTrue(emptyArticle.getEnviron().getSchemes().isEmpty());
         Assertions.assertTrue(emptyArticle.getEnviron().getRequirements().isEmpty());
         Assertions.assertTrue(emptyArticle.getTextItems().isEmpty());
+    }
+
+    @Test
+    public void testEmptyArticleWrong() throws IOException {
+        String emptyArticleStr = Parser.loadFromFile("/fixtures/articles/emptyArticleWrongOrder.miz");
+        try {
+            new Parser().parse("EMPTY", emptyArticleStr);
+            Assertions.fail("Wrong order of 'environ' and 'begin' must cause a ParserException!");
+        } catch (ParseException ex) {
+            // expected
+        }
     }
 
 }
